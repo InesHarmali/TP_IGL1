f316e42814b40760a6db8b24c3efda9d524092bd
InesHarmali <32884781+InesHarmali@users.noreply.github.com>
2017-10-19 15:08:26 +0200

    rechanger les bibliothèques importée

--- StringHelper.java
+++ StringHelper.java
@@ -1,5 +1,6 @@
   package tp_igl;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Cette class contient des fonctions qui s'appliquent aux chaines de caractére  
@@ -152,8 +153,13 @@ public class StringHelper {
         return occ;
        }
     } 
+  
+  /**
+     *Cette fonction elimine tous les occurence des mots vide definis par[ou,et,à,non] dans un texte donné (String) 
+     * @param chaine c'est le texte dont vous voulez supprimer les mots vide
+     * @return cette fonction retourne un texte qui ne conteint pas les mots vide ou,et,à,non
+     */
    public static  String EliminationMotvide( String chaine) {
-
 
         String s =" ";
         char c= s.charAt(0);
@@ -171,12 +177,9 @@ public class StringHelper {
             i++;
 
         }
-
         String chaineF = "";
-
         for (int m = 0; m < list.size(); m++) {
             chaineF = chaineF + list.get(m)+" ";
-
         }
         return chaineF;
     }
